f18cc625bd4726467663f433fd6054597f05339a
leehj050211 <leehj050211@gmail.com>
2022-02-22 21:24:57 +0900

    Add: 파일 id 숨기기, Fix: 모바일 스크롤 버그 및 토스트 메시지 위치

--- public/js/common.js
+++ public/js/common.js
@@ -1,5 +1,7 @@
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
+    vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
 const $ = document.querySelector.bind(document)
